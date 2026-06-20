5c969728519f747ca717cb4d01c54414346d8da3
olivercarmonaruby-sys <olivercarmona.ruby@prepaandresquintanaroo.edu.mx>
2026-06-20 08:36:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script para manejar el formulario de contacto
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Gracias por tu mensaje. Pronto me pondré en contacto contigo.");
+});
